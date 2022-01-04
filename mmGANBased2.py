64534e5c6eda48aebd6c305b8785c69897a321f6
André Filipe Sousa Ferreira <55408616+ShadowTwin41@users.noreply.github.com>
2022-01-04 14:04:45 +0000

    Add files via upload

---
+++ mmGANBased2.py
@@ -0,0 +1,111 @@
+import numpy as np
+import torch
+import os
+from torch import nn
+from torch import optim
+
+from torch.nn import functional as F
+from torch.nn.utils import spectral_norm as SN
+#***********************************************
+#Encoder and Discriminator has same architecture
+#***********************************************
+
+'''
+Base architecture->Model_alphaWGAN.py
+######################################--Improvements--######################################
+#Instead of use BatchNorm3d, It is used InstanceNorm3d
+#Application of Spectral Normalization (SN) after each convolution (based on https://arxiv.org/pdf/1802.05957.pdf)
+
+treinar para vetores de tamanho 1000 também, para comparar.
+'''
+class Discriminator(nn.Module):
+    def __init__(self, channel=512,out_class=1,is_dis =True):
+        super(Discriminator, self).__init__()
+        self.is_dis=is_dis
+        self.channel = channel
+        n_class = out_class 
+        
+        self.conv1 = SN(nn.Conv3d(1, channel//8, kernel_size=4, stride=2, padding=1))
+        self.conv2 = SN(nn.Conv3d(channel//8, channel//4, kernel_size=4, stride=2, padding=1))
+        self.bn2 = nn.InstanceNorm3d(channel//4)
+        self.conv3 = SN(nn.Conv3d(channel//4, channel//2, kernel_size=4, stride=2, padding=1))
+        self.bn3 = nn.InstanceNorm3d(channel//2)
+        self.conv4 = SN(nn.Conv3d(channel//2, channel, kernel_size=4, stride=2, padding=1))
+        self.bn4 = nn.InstanceNorm3d(channel)
+        self.conv5 = SN(nn.Conv3d(channel, n_class, kernel_size=4, stride=1, padding=0))
+        
+    def forward(self, x, _return_activations=False):
+        h1 = F.leaky_relu(self.conv1(x), negative_slope=0.2)
+        h2 = F.leaky_relu(self.bn2(self.conv2(h1)), negative_slope=0.2)
+        h3 = F.leaky_relu(self.bn3(self.conv3(h2)), negative_slope=0.2)
+        h4 = F.leaky_relu(self.bn4(self.conv4(h3)), negative_slope=0.2)
+        h5 = self.conv5(h4)
+        output = h5
+        
+        return output
+    
+class Code_Discriminator(nn.Module):
+    def __init__(self, code_size=100,num_units=750):
+        super(Code_Discriminator, self).__init__()
+        n_class = 1
+        self.l1 = nn.Sequential(SN(nn.Linear(code_size, num_units)),
+                                nn.BatchNorm1d(num_units),
+                                nn.LeakyReLU(0.2,inplace=True))
+        self.l2 = nn.Sequential(SN(nn.Linear(num_units, num_units)),
+                                nn.BatchNorm1d(num_units),
+                                nn.LeakyReLU(0.2,inplace=True))
+        self.l3 = SN(nn.Linear(num_units, 1))
+        
+    def forward(self, x):
+        h1 = self.l1(x)
+        h2 = self.l2(h1)
+        h3 = self.l3(h2)
+        output = h3
+            
+        return output
+
+class Generator(nn.Module):
+    def __init__(self, noise:int=100, channel:int=64):
+        super(Generator, self).__init__()
+        _c = channel
+
+        self.leaky_relu = nn.LeakyReLU()
+        self.noise = noise
+        self.tp_conv1 = SN(nn.ConvTranspose3d(noise, _c*8, kernel_size=4, stride=1, padding=0, bias=False))
+        self.bn1 = nn.InstanceNorm3d(_c*8)
+        
+        self.tp_conv2 = SN(nn.Conv3d(_c*8, _c*4, kernel_size=3, stride=1, padding=1, bias=False))
+        self.bn2 = nn.InstanceNorm3d(_c*4)
+        
+        self.tp_conv3 = SN(nn.Conv3d(_c*4, _c*2, kernel_size=3, stride=1, padding=1, bias=False))
+        self.bn3 = nn.InstanceNorm3d(_c*2)
+        
+        self.tp_conv4 = SN(nn.Conv3d(_c*2, _c, kernel_size=3, stride=1, padding=1, bias=False))
+        self.bn4 = nn.InstanceNorm3d(_c)
+        
+        self.tp_conv5 = SN(nn.Conv3d(_c, 1, kernel_size=3, stride=1, padding=1, bias=False))
+        
+    def forward(self, noise):
+
+        noise = noise.view(-1,self.noise,1,1,1)
+        h = self.tp_conv1(noise)
+        h = self.leaky_relu(self.bn1(h))
+        
+        h = nn.functional.interpolate(h,scale_factor = 2)
+        h = self.tp_conv2(h)
+        h = self.leaky_relu(self.bn2(h))
+     
+        h = nn.functional.interpolate(h,scale_factor = 2)
+        h = self.tp_conv3(h)
+        h = self.leaky_relu(self.bn3(h))
+
+        h = nn.functional.interpolate(h,scale_factor = 2)
+        h = self.tp_conv4(h)
+        h = self.leaky_relu(self.bn4(h))
+
+        h = nn.functional.interpolate(h,scale_factor = 2)
+        h = self.tp_conv5(h)
+
+        h = torch.tanh(h)
+
+        return h
